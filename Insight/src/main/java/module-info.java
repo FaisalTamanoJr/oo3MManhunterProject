8d83968cb08abd8cc739a82862bce16b68b05af8
FaisalRichardJr <faisalrichardjr@gmail.com>
2023-07-24 22:10:38 +0800

    remaking the code from scratch

--- Insight/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.insight.insight {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.insight.insight to javafx.fxml;
-    exports com.insight.insight;
-}
